2ab8bed949338cbd29a1dda1cf1c9724bcbab6a2
Curtis Kettler <curtiskettler@gmail.com>
2021-08-28 15:34:08 -0500

    clicking on movie poster takes user to new page based on id

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 import { Grid } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 
 function MovieList() {
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -14,6 +16,7 @@ function MovieList() {
 
     const onMovieClick = () => {
         console.log('clicked on a movie');
+        history.push(`/details/:id`)
     }
 
     return (
