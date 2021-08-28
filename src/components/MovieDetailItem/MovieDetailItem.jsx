a11ffeccdca2c2a71984d25630bd9544336037db
Curtis Kettler <curtiskettler@gmail.com>
2021-08-28 18:20:11 -0500

    return to movie list button added and working

--- src/components/MovieDetailItem/MovieDetailItem.jsx
+++ src/components/MovieDetailItem/MovieDetailItem.jsx
@@ -1,6 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+
 function MovieDetailItem() {
+    const movies = useSelector(store => store.movies);
+
     return (
+        <>
         <h1>and some more details...</h1>
+        </>
     )
 }
 
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,14 @@
 import MovieDetailItem from "../MovieDetailItem/MovieDetailItem";
+import { Link } from 'react-router-dom';
 
 function MovieDetails() {
+
+
 
     return (
         <>
         <h1>Here are some movie details!!!</h1>
+        <Link to='/'><button>Return to movie list</button></Link>
         <MovieDetailItem />
         </>
     )
